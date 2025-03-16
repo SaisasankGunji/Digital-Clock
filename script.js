ca188a894ef4e9a21b6890c6f1b8d505c38d518d
Sai sasank Gunji <saisasnk.gunji@gmail.com>
2025-03-16 15:49:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-function currentTime(){
-const date=new Date();
-let hrs=date.getHours();
-let mins=date.getMinutes().toString().padStart(2,0);
-let secs=date.getSeconds().toString().padStart(2,0);
-let meridiem=hrs>=12?"PM":"AM";
-hrs=hrs%12||12;
-hrs=hrs.toString().padStart(2,0);
-const timeString=`${hrs}:${mins}:${secs} ${meridiem}`;
-document.getElementById("clock-display").textContent=timeString;
+function currentTime() {
+  const date = new Date();
+  let hrs = date.getHours();
+  let mins = date.getMinutes().toString().padStart(2, 0);
+  let secs = date.getSeconds().toString().padStart(2, 0);
+  let meridiem = hrs >= 12 ? "PM" : "AM";
+  hrs = hrs % 12 || 12;
+  hrs = hrs.toString().padStart(2, 0);
+  const timeString = `${hrs}:${mins}:${secs} ${meridiem}`;
+  document.getElementById("clock-display").textContent = timeString;
 }
 
 currentTime();
 setInterval(currentTime, 1000);
+
 
 
 
